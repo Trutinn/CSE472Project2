f3fbd2de433fa43687cb22eb250944e209259333
Trutinn <pmott@asu.edu>
2020-11-10 19:17:04 -0800

    New model completed

--- dataManipulation.py
+++ dataManipulation.py
@@ -6,11 +6,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.readwrite import json_graph
 import os
-from karateclub import Graph2Vec
 import pandas as pd
+from karateclub import Graph2Vec
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import auc, accuracy_score, confusion_matrix, mean_squared_error
-import xgboost as xgb
+from sklearn.linear_model import LogisticRegression
+import random
 
 consumer_key = '2qGxcEviGiPDBg026BGAJPwR2'
 consumer_secret = 'vYdce0YD6mBitSOcOf0c9OcHWkJVFf3hjjMi5lBBHEKKq6SNd0'
@@ -20,12 +21,27 @@ access_token_secret = 'aTC8bo4Gov5svtEmu68ULNJDHiDCoiMWRR05tHcKc5mr7'
 t = Twarc(consumer_key, consumer_secret, access_token, access_token_secret)
 
 
-def timelineCollection(userIDArg, option):  # option: 0 for bot, 1 for verified
+def csvToTimeline(filePath):
+    counter = 1
+    with open(filePath, 'r') as f:
+        for line in f:
+            print(counter)
+            counter += 1
+            splitLine = line.split(',')
+            id = splitLine[0].strip()
+            label = splitLine[1].strip()
+            if label == "bot":
+                timelineCollection(id, 0)
+            elif label == "human":
+                timelineCollection(id, 1)
+
+
+def timelineCollection(userIDArg, option):  # option: 0 for bot, 1 for verified  # MUST CHANGE ABSOLUTE PATHS
     if option == 0 or option == 1:
         if option == 0:
-            fileName = "accountInfo/botTimeline/timeline" + str(userIDArg).strip() + ".jsonl"
+            fileName = "E:/Users/Preston/CSE472P2AccountInfo/botTimeline/timeline" + str(userIDArg).strip() + ".jsonl"
         elif option == 1:
-            fileName = "accountInfo/verifiedTimeline/timeline" + str(userIDArg).strip() + ".jsonl"
+            fileName = "E:/Users/Preston/CSE472P2AccountInfo/verifiedTimeline/timeline" + str(userIDArg).strip() + ".jsonl"
         with open(fileName, 'w') as f:
             for line in t.timeline(user_id=int(userIDArg)):
                 record = json.dumps(line)
@@ -103,11 +119,15 @@ def bulkJsonCreation(idFile, option):  # option: 0 for bot, 1 for verified
 def bulkGraphCreation(timelineDir, option):  # option: 0 for bot, 1 for verified
     counter = 1
     for file in os.listdir(timelineDir):
-        userID = file.replace("timeline", "")
-        userID = userID.replace(".jsonl", "")
-        parsedData = parseTimeline(timelineDir + file)
-        GTemp = graphCreation(parsedData)
-        saveGraph(GTemp, userID, option)
+        if os.stat(timelineDir + file).st_size == 0:
+            print(timelineDir + file, " removed for being empty.")
+            os.remove(timelineDir + file)
+        else:
+            userID = file.replace("timeline", "")
+            userID = userID.replace(".jsonl", "")
+            parsedData = parseTimeline(timelineDir + file)
+            GTemp = graphCreation(parsedData)
+            saveGraph(GTemp, userID, option)
         print(counter)
         counter += 1
     print("done")
@@ -133,73 +153,104 @@ def mergeDataset(datasetDir):  # merge all csv datasets for training and testing
                         fw.write(line)
 
 
-def predictOn1Graph(filePath):
-    GTemp = loadGraph(filePath)
-    convertGTemp = nx.convert_node_labels_to_integers(GTemp)
-    model = Graph2Vec(dimensions=64)
-    unDirconvertedGTemp = convertGTemp.to_undirected()
-    model.fit([unDirconvertedGTemp])
-    modelFrame = pd.DataFrame(model.get_embedding())
-
-    classification_model = xgb.XGBClassifier(objective="binary:logistic", random_state=42, learning_rate=0.05,
-                                             n_estimators=5000, early_stopping_rounds=10)
-    classification_model.load_model("data/graph_classifier_3.json")
-    print("Predicting...")
-    pred = classification_model.predict(modelFrame)
-
-    print("User: " + pred[0])
-
-
-# bulkJsonCreation("data/botwikiID.txt", 0)
-
-# bulkGraphCreation("accountInfo/botTimeline/", 0)
-
-# JSONtoGraph("accountInfo/botGraphs/")
-
-# mergeDataset("twitterdataset/")
+def convertGraphToVec(filePath):  # returns 0 on FileNotFoundError, returns 1 if graph JSON is empty, returns graph model on no error
+    try:
+        GTemp = loadGraph(filePath)
+    except FileNotFoundError:
+        return 0
+    if nx.is_empty(GTemp):
+        os.remove(filePath)
+        return 1
+    else:
+        convertGTemp = nx.convert_node_labels_to_integers(GTemp)
+        model = Graph2Vec(dimensions=64)
+        unDirconvertedGTemp = convertGTemp.to_undirected()
+        try:
+            model.fit([unDirconvertedGTemp])
+            modelFrame = pd.DataFrame(model.get_embedding())
+            return modelFrame
+        except RuntimeError:
+            return 2
 
 
-# colList = ["id", "label"]
-# dataset = pd.read_csv("twitterdataset/mergedDataset.csv", usecols=colList)
-# X = dataset.drop(columns=["label"])
-# Y = dataset["label"]
-#
-# seed = 42
-# test_size = .2
-# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
-#
-# classification_model = xgb.XGBClassifier(objective="binary:logistic", random_state=42, learning_rate=0.05, n_estimators=5000,
-#                           early_stopping_rounds=10)
-# classification_model.fit(X_train, y_train)
-#
-# y_pred = classification_model.predict(X_test)
-# accuracy = accuracy_score(y_test, y_pred)
-# print(accuracy)
-#
-# predictOn1Graph("accountInfo/botGraphs/960538579993878529.json")
+def bulkGraphsToFeatures():
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    with open("data/graphDataNew.csv", 'w') as fw:
+        with open("data/mergedDatasetNew.csv", 'r') as f:
+            counter = 1
+            for line in f:
+                print(counter)
+                counter += 1
+                splitLine = line.split(",")
+                id = splitLine[0]
+                label = splitLine[1]
+                if label.strip() == "human":
+                    graphFile = "accountInfo/verifiedGraphs/" + id + ".json"
+                elif label.strip() == "bot":
+                    graphFile = "accountInfo/botGraphs/" + id + ".json"
+                else:
+                    print("WRONG LABEL")
+                    break
 
-GTemp = loadGraph("accountInfo/botGraphs/12112272.json")
-convertGTemp = nx.convert_node_labels_to_integers(GTemp)
-model = Graph2Vec(dimensions=64)
-unDirconvertedGTemp = convertGTemp.to_undirected()
-model.fit([unDirconvertedGTemp])
-modelFrame = pd.DataFrame(model.get_embedding())
+                modelFrame = convertGraphToVec(graphFile)
+                if modelFrame is 0:
+                    print("File not found")
+                elif modelFrame is 1:
+                    print("File removed for being empty")
+                elif modelFrame is 2:
+                    print("Bad file")
+                else:
+                    fw.write(label.strip() + ",")
+                    values = modelFrame.values
+                    for i in range(0, len(values[0])):
+                        fw.write(str(values[0][i]))
+                        if i != len(values[0]) - 1:
+                            fw.write(",")
+                    fw.write("\n")
 
-mergedDataset = pd.read_csv("twitterdataset/mergedDataset.csv")
-X = mergedDataset.drop(columns=['label'])
-Y = mergedDataset['label']
 
-seed = 42
-test_size = 0.2
-X_train, X_test, y_train, y_test = train_test_split(X.values, Y.values, test_size=test_size, random_state=seed)
+# CHANGE ABSOLUTE PATHS
 
-classModel = xgb.XGBClassifier(objective="binary:logistic", random_state=42, learning_rate=0.05, n_estimators=5000, early_stopping_rounds=10)
-classModel.fit(X_train, y_train)
 
-y_pred = classModel.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
-print(modelFrame)
-print(modelFrame.values)
-pred = classModel.predict(modelFrame)
-print("USER" + ': ' + pred[0])
+# TwitterID -> timeLine -> graph -> featureVector -> LR(featureVector) -> bot/human
+# TwitterID Datasets -> Merged Into CSV -> Create timeline.jsonl files for each ID -> Create a graph.jsonl for each ID -> Turn each graph into features and store in CSV -> Train model on that CSV
+
+# TwitterID Dataset -> csvToTimeline(dataset)  (creates a bunch of timeline files) ->
+# -> bulkGraphCreation(dir, option) option = 0 for bot dir and 1 for human dir (creates a bunch of graph files) -> bulkGraphsToFeatures() (creates large csv file of graph features) -> shuffle feature.csv ->
+# -> makeModel(featureCSV) -> output (bot/human)
+
+
+#makeLargeCsv()
+
+# convertTsvToCsv("temp/")
+# bulkJsonCreation("E:/Users/Preston/CSE472P2AccountInfo/botTimeline/",0)
+# csvToTimeline("temp/midterm-2018.csv")
+# bulkGraphCreation("accountInfo/verifiedGraphs/", 1)
+# for file in os.listdir("accountInfo/botGraphs/"):
+#     convertGraphToVec("accountInfo/botGraphs/"+file)
+
+
+def fileLen(fileName):
+    with open(fileName) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+dataset = pd.read_csv('data/randGraphDataNew.csv', sep=',',header = 0)
+dataset.head()
+X = dataset.iloc[:, 1:]
+Y = dataset.iloc[:, 0]
+
+print(X)
+print(Y)
+
+LR = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr').fit(X, Y)
+with open('data/randGraphDataNew.csv', 'r') as f:
+    with open('data/output.txt', 'w') as fw:
+        for i in range(0, fileLen('data/randGraphDataNew.csv')-1):
+            if Y[i].strip() == 'bot':
+                fw.write("BOT: " + str(LR.predict(X.iloc[i:, :])) + "\n")
+            else:
+                fw.write("HUMAN: " + str(LR.predict(X.iloc[i:, :])) + "\n")
+
+
